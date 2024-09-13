e3a57a9c700e5aefb3453972ff064c74a17849d3
zoe <zoe27@126.com>
2024-09-13 09:30:35 +0800

    nft update

--- learning/src/Swapnft.sol
+++ learning/src/Swapnft.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
- 
 
 contract Swapnft is ERC721{
 
@@ -12,10 +11,11 @@ contract Swapnft is ERC721{
 
     address payable public owner;
 
-    constructor(uint _price, uint _token_id){
+    constructor(uint _price, uint _token_id) ERC721("SwapNFT", "SNFT") {
         tokenId = _token_id;
         price = _price;
         owner = payable(msg.sender);
+        _mint(msg.sender, tokenId);
     }
 
     function updatePrice(uint _new_price) external{
@@ -28,10 +28,11 @@ contract Swapnft is ERC721{
         selfdestruct(owner);
     }
 
-    function purchase() external{
+    function purchase() payable external{
         require(msg.value == price);
-        owner.call{value: msg.value}();
+        // owner.call{value: msg.value}("");
+        owner.transfer(msg.value);
         transferFrom(owner, msg.sender, tokenId);
-        selfdestruct(owner);
+        // selfdestruct(owner);
     }
 }
